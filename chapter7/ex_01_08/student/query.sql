14d398825c239261e85d42fe1c88bb9808d521cb
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-03-30 00:12:02 +0000

    Testing Mon Mar 30 2026 00:12:02 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_01_08/student/query.sql
+++ chapter7/ex_01_08/student/query.sql
@@ -1,4 +1,5 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
-SELECT DISTINCT PROJ_NUM
+SELECT ASSIGN_NUM,EMP_NUM,PROJ_NUM,ASSIGN_CHARGE,
+ROUND(ASSIGN_CHG_HR * ASSIGN_HOURS, 2) AS CALC_ASSIGN_CHARGE
 FROM ASSIGNMENT
-ORDER BY PROJ_NUM
+ORDER BY ASSIGN_NUM;
