508659ce3666c5ddaabc34d306e4624799057cb8
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-04-29 20:47:17 +0000

    Testing Wed Apr 29 2026 20:47:17 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,3 +1,3 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
-SELECT COUNT(DISTINCT INV_NUMBER) AS total_invoices
+SELECT COUNT(DISTINCT INV_NUMBER)
 FROM INVOICE;
