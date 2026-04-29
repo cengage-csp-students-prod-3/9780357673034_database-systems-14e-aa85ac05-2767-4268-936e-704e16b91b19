e9c2029f66150028457acf9356ac140e3075ca79
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-04-29 21:56:16 +0000

    Testing Wed Apr 29 2026 21:56:16 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,10 +1,8 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.SELECT 
 SELECT
-    ROUND(SUM(C.CUS_BALANCE), 2) AS `Total Balance`,
-    MIN(C.CUS_BALANCE) AS `Minimum Balance`,
-    MAX(C.CUS_BALANCE) AS `Maximum Balance`,
-    ROUND(AVG(C.CUS_BALANCE), 2) AS `Average Balance`
-FROM CUSTOMER C
-WHERE C.CUS_CODE NOT IN (
-    SELECT CUS_CODE FROM INVOICE
-);
+    P.P_DESCRIPT,
+    P.P_QOH,
+    P.P_PRICE,
+    ROUND(P.P_QOH * P.P_PRICE, 2) AS `Subtotal`
+FROM PRODUCT P
+ORDER BY `Subtotal` DESC;
