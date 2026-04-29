45f026e1bb2b63bfa6d493de1f2adef290c1d5cd
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-04-29 21:54:47 +0000

    Testing Wed Apr 29 2026 21:54:47 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,9 +1,10 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.SELECT 
 SELECT
-    C.CUS_CODE,
-    C.CUS_BALANCE
+    ROUND(SUM(C.CUS_BALANCE), 2) AS `Total Balance`,
+    MIN(C.CUS_BALANCE) AS `Minimum Balance`,
+    MAX(C.CUS_BALANCE) AS `Maximum Balance`,
+    ROUND(AVG(C.CUS_BALANCE), 2) AS `Average Balance`
 FROM CUSTOMER C
 WHERE C.CUS_CODE NOT IN (
     SELECT CUS_CODE FROM INVOICE
-)
-ORDER BY C.CUS_CODE;
+);
