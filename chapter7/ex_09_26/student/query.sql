338cf55f7cf518bafdbf21a949d453d557858ad4
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-04-29 21:07:03 +0000

    Testing Wed Apr 29 2026 21:07:03 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,14 +1,17 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
 SELECT 
-    C.CUS_CODE,
-    I.INV_NUMBER,
-    ROUND(SUM(L.LINE_UNITS * L.LINE_PRICE), 2) AS `INVOICE TOTAL`
-FROM CUSTOMER C
-JOIN INVOICE I ON C.CUS_CODE = I.CUS_CODE
-JOIN LINE L ON I.INV_NUMBER = L.INV_NUMBER
-GROUP BY 
-    C.CUS_CODE,
-    I.INV_NUMBER
-ORDER BY 
-    C.CUS_CODE,
-    I.INV_NUMBER;
+    COUNT(*) AS `TOTAL INVOICES`,
+    ROUND(SUM(L.LINE_UNITS * L.LINE_PRICE), 2) AS `TOTAL SALES`,
+    ROUND(MIN(customer_total), 2) AS `MINIMUM CUSTOMER PURCHASES`,
+    ROUND(MAX(customer_total), 2) AS `LARGEST CUSTOMER PURCHASES`,
+    ROUND(AVG(customer_total), 2) AS `AVERAGE CUSTOMER PURCHASES`
+FROM (
+    SELECT 
+        C.CUS_CODE,
+        SUM(L.LINE_UNITS * L.LINE_PRICE) AS customer_total
+    FROM CUSTOMER C
+    JOIN INVOICE I ON C.CUS_CODE = I.CUS_CODE
+    JOIN LINE L ON I.INV_NUMBER = L.INV_NUMBER
+    GROUP BY C.CUS_CODE
+) AS sub
+JOIN LINE L ON 1=1;
