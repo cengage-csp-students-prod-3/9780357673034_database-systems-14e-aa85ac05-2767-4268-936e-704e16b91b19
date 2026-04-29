1fac6e3fd453d2c5685e615abb96445f043b6cf3
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-04-29 21:46:01 +0000

    Testing Wed Apr 29 2026 21:46:01 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,9 +1,7 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.SELECT SELE
-SELECT
-    I.CUS_CODE,
-    COUNT(DISTINCT I.INV_NUMBER) AS `Number of Invoices`,
-    ROUND(SUM(L.LINE_UNITS * L.LINE_PRICE), 2) AS `Total Customer Purchases`
-FROM INVOICE I
-JOIN LINE L ON I.INV_NUMBER = L.INV_NUMBER
-GROUP BY I.CUS_CODE
-ORDER BY I.CUS_CODE;
+SELECT DISTINCT
+    C.CUS_CODE,
+    C.CUS_BALANCE
+FROM CUSTOMER C
+JOIN INVOICE I ON C.CUS_CODE = I.CUS_CODE
+ORDER BY C.CUS_CODE;
