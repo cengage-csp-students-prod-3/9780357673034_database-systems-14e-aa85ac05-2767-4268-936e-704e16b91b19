c17f84e9605170f32d050e3f045095dca11a8480
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-04-29 20:50:36 +0000

    Testing Wed Apr 29 2026 20:50:36 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,3 +1,16 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
-SELECT COUNT(*)
-FROM INVOICE;
+SELECT 
+    C.CUS_CODE,
+    I.INV_NUMBER,
+    I.INV_DATE,
+    P.P_DESCRIPT,
+    L.LINE_UNITS,
+    L.LINE_PRICE
+FROM CUSTOMER C
+JOIN INVOICE I ON C.CUS_CODE = I.CUS_CODE
+JOIN LINE L ON I.INV_NUMBER = L.INV_NUMBER
+JOIN PRODUCT P ON L.P_CODE = P.P_CODE
+ORDER BY 
+    C.CUS_CODE,
+    I.INV_NUMBER,
+    P.P_DESCRIPT;
