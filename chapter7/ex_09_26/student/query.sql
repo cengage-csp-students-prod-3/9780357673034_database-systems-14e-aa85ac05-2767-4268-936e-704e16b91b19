5a38634470de19916fe9f85081e889794a7a41e8
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-04-29 21:25:53 +0000

    Testing Wed Apr 29 2026 21:25:53 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,10 +1,4 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.SELECT 
-SELECT
-    MIN(CUS_BALANCE) AS `MINIMUM BALANCE`,
-    MAX(CUS_BALANCE) AS `MAXIMUM BALANCE`,
-    ROUND(AVG(CUS_BALANCE), 2) AS `AVERAGE BALANCE`
-FROM (
-    SELECT DISTINCT C.CUS_CODE, C.CUS_BALANCE
-    FROM CUSTOMER C
-    JOIN INVOICE I ON C.CUS_CODE = I.CUS_CODE
-) AS X;
+SELECT COUNT(*) AS COUNT
+FROM CUSTOMER
+WHERE CUS_BALANCE > 500;
