d720f5cd46a4b4b93ec91175d78d66b4492adb5a
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-04-29 21:50:16 +0000

    Testing Wed Apr 29 2026 21:50:16 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,7 +1,7 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.SELECT SELE
 SELECT
-    MIN(C.CUS_BALANCE) AS `Minimum Balance`,
-    MAX(C.CUS_BALANCE) AS `Maximum Balance`,
-    ROUND(AVG(C.CUS_BALANCE), 2) AS `Average Balance`
-FROM CUSTOMER C
-JOIN INVOICE I ON C.CUS_CODE = I.CUS_CODE;
+    ROUND(SUM(CUS_BALANCE), 2) AS `Total Balance`,
+    MIN(CUS_BALANCE) AS `Minimum Balance`,
+    MAX(CUS_BALANCE) AS `Maximum Balance`,
+    ROUND(AVG(CUS_BALANCE), 2) AS `Average Balance`
+FROM CUSTOMER;
