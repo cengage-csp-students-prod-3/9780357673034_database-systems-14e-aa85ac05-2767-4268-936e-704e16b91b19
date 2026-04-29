968e36e4045d367d62e9921614e6388a9f1dbf6c
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-04-29 20:52:19 +0000

    Testing Wed Apr 29 2026 20:52:19 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,16 +1,13 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
 SELECT 
     C.CUS_CODE,
-    I.INV_NUMBER,
-    I.INV_DATE,
-    P.P_DESCRIPT,
-    L.LINE_UNITS,
-    L.LINE_PRICE
+    C.CUS_BALANCE,
+    SUM(L.LINE_UNITS * L.LINE_PRICE) AS "Total Purchases"
 FROM CUSTOMER C
 JOIN INVOICE I ON C.CUS_CODE = I.CUS_CODE
 JOIN LINE L ON I.INV_NUMBER = L.INV_NUMBER
-JOIN PRODUCT P ON L.P_CODE = P.P_CODE
-ORDER BY 
+GROUP BY 
     C.CUS_CODE,
-    I.INV_NUMBER,
-    P.P_DESCRIPT;
+    C.CUS_BALANCE
+ORDER BY 
+    C.CUS_CODE;
