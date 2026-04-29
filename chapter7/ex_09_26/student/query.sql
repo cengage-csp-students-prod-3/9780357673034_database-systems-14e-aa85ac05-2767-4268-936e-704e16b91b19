1d918055eb95e74d91ec470b093d3d2d58a111cc
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-04-29 21:38:07 +0000

    Testing Wed Apr 29 2026 21:38:07 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,11 +1,8 @@
--- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.SELECT 
+-- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.SELECT SELE
 SELECT
-    C.CUS_CODE,
-    C.CUS_BALANCE,
-    ROUND(SUM(L.LINE_UNITS * L.LINE_PRICE), 2) AS `Total Purchases`,
-    COUNT(*) AS `Number of Purchases`
-FROM CUSTOMER C
-JOIN INVOICE I ON C.CUS_CODE = I.CUS_CODE
+    I.INV_NUMBER,
+    ROUND(SUM(L.LINE_UNITS * L.LINE_PRICE), 2) AS `Invoice Total`
+FROM INVOICE I
 JOIN LINE L ON I.INV_NUMBER = L.INV_NUMBER
-GROUP BY C.CUS_CODE, C.CUS_BALANCE
-ORDER BY C.CUS_CODE;
+GROUP BY I.INV_NUMBER
+ORDER BY I.INV_NUMBER;
