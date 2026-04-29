5b4ef12197f98c6f6ee6d2dbccca7c523a0099a7
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-04-29 21:33:16 +0000

    Testing Wed Apr 29 2026 21:33:16 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,4 +1,16 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.SELECT 
-SELECT COUNT(*) 
-FROM CUSTOMER
-WHERE CUS_BALANCE > 500;
+SELECT 
+    C.CUS_CODE,
+    I.INV_NUMBER,
+    P.P_DESCRIPT,
+    L.LINE_UNITS AS `Units Bought`,
+    L.LINE_PRICE AS `Unit Price`,
+    ROUND(L.LINE_UNITS * L.LINE_PRICE, 2) AS `Subtotal`
+FROM CUSTOMER C
+JOIN INVOICE I ON C.CUS_CODE = I.CUS_CODE
+JOIN LINE L ON I.INV_NUMBER = L.INV_NUMBER
+JOIN PRODUCT P ON L.P_CODE = P.P_CODE
+ORDER BY 
+    C.CUS_CODE,
+    I.INV_NUMBER,
+    P.P_DESCRIPT;
