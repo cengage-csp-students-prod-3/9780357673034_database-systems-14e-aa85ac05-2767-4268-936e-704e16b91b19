fa031c34f9e1f9946ee2126e85a883ef2b152c28
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-04-29 21:27:21 +0000

    Testing Wed Apr 29 2026 21:27:20 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,4 +1,4 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.SELECT 
-SELECT COUNT(*) AS COUNT
+SELECT COUNT(*) 
 FROM CUSTOMER
 WHERE CUS_BALANCE > 500;
