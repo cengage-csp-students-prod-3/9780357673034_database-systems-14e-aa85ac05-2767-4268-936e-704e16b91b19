92aadeeadff285a1813fc9987f3fe3bc3a901d55
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-04-29 21:08:09 +0000

    Testing Wed Apr 29 2026 21:08:09 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,6 +1,6 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.
 SELECT 
-    COUNT(*) AS `TOTAL INVOICES`,
+    (SELECT COUNT(*) FROM INVOICE) AS `TOTAL INVOICES`,
     ROUND(SUM(L.LINE_UNITS * L.LINE_PRICE), 2) AS `TOTAL SALES`,
     ROUND(MIN(customer_total), 2) AS `MINIMUM CUSTOMER PURCHASES`,
     ROUND(MAX(customer_total), 2) AS `LARGEST CUSTOMER PURCHASES`,
@@ -13,5 +13,4 @@ FROM (
     JOIN INVOICE I ON C.CUS_CODE = I.CUS_CODE
     JOIN LINE L ON I.INV_NUMBER = L.INV_NUMBER
     GROUP BY C.CUS_CODE
-) AS sub
-JOIN LINE L ON 1=1;
+) AS sub;
