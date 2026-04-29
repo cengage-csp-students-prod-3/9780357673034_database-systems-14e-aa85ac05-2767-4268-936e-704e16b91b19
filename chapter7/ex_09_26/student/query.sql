7e4c65418e900d4aae0441e7cd4d0134853cca2f
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-04-29 21:57:15 +0000

    Testing Wed Apr 29 2026 21:57:15 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_09_26/student/query.sql
+++ chapter7/ex_09_26/student/query.sql
@@ -1,8 +1,4 @@
 -- Write your query below and then click "Run Query" to execute it. To save multiple queries, click the "+" icon on the left.SELECT 
 SELECT
-    P.P_DESCRIPT,
-    P.P_QOH,
-    P.P_PRICE,
-    ROUND(P.P_QOH * P.P_PRICE, 2) AS `Subtotal`
-FROM PRODUCT P
-ORDER BY `Subtotal` DESC;
+    ROUND(SUM(P_QOH * P_PRICE), 2) AS `Total Value of Inventory`
+FROM PRODUCT;
