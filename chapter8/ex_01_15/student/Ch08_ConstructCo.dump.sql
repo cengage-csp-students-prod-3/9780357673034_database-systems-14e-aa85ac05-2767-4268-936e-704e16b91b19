8ae958ce6fb06d4f9766a826845f57d11170500b
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-05-01 19:09:14 +0000

    Testing Fri May 01 2026 19:09:14 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex_01_15/student/Ch08_ConstructCo.dump.sql
+++ chapter8/ex_01_15/student/Ch08_ConstructCo.dump.sql
@@ -106,7 +106,7 @@ CREATE TABLE `EMP_1` (
 
 LOCK TABLES `EMP_1` WRITE;
 /*!40000 ALTER TABLE `EMP_1` DISABLE KEYS */;
-INSERT INTO `EMP_1` VALUES ('101','News','John','G','2000-11-08','502'),('102','Senior','David','H','1989-07-12','501');
+INSERT INTO `EMP_1` VALUES ('101','News','John','G','2000-11-08','502'),('102','Senior','David','H','1989-07-12','501'),('103','Arbough','June','E','2000-12-01','500'),('104','Ramoras','Anne','K','1991-11-15','501'),('105','Johnson','Alice','K','1997-02-01','502'),('106','Smithfield','William',NULL,'2008-06-22','500'),('107','Alonzo','Maria','D','1997-10-10','500'),('108','Washington','Ralph','B','1995-08-22','501'),('109','Smith','Larry','W','2001-07-18','501');
 /*!40000 ALTER TABLE `EMP_1` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -178,4 +178,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-05-01 19:06:23
+-- Dump completed on 2026-05-01 19:09:10
