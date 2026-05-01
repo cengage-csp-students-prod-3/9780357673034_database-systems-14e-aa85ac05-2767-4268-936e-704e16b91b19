d4f60be3a2fafc56425d92a9e9774ff8de3ec9b7
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-05-01 19:24:56 +0000

    Testing Fri May 01 2026 19:24:56 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex_01_15/student/Ch08_ConstructCo.dump.sql
+++ chapter8/ex_01_15/student/Ch08_ConstructCo.dump.sql
@@ -123,7 +123,9 @@ CREATE TABLE `EMP_2` (
   `EMP_FNAME` varchar(15) NOT NULL,
   `EMP_INITIAL` varchar(1) DEFAULT NULL,
   `EMP_HIREDATE` date DEFAULT NULL,
-  `JOB_CODE` varchar(3) DEFAULT NULL
+  `JOB_CODE` varchar(3) DEFAULT NULL,
+  `EMP_PCT` decimal(4,2) DEFAULT NULL,
+  `PROJ_NUM` char(3) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -133,7 +135,7 @@ CREATE TABLE `EMP_2` (
 
 LOCK TABLES `EMP_2` WRITE;
 /*!40000 ALTER TABLE `EMP_2` DISABLE KEYS */;
-INSERT INTO `EMP_2` VALUES ('101','News','John','G','2000-11-08','502'),('102','Senior','David','H','1989-07-12','501'),('103','Arbough','June','E','2000-12-01','500'),('104','Ramoras','Anne','K','1991-11-15','501'),('105','Johnson','Alice','K','1997-02-01','502'),('106','Smithfield','William',NULL,'2008-06-22','500'),('107','Alonzo','Maria','D','1997-10-10','501'),('108','Washington','Ralph','B','1995-08-22','501'),('109','Smith','Larry','W','2001-07-18','501');
+INSERT INTO `EMP_2` VALUES ('101','News','John','G','2000-11-08','502',NULL,NULL),('102','Senior','David','H','1989-07-12','501',NULL,NULL),('103','Arbough','June','E','2000-12-01','500',NULL,NULL),('104','Ramoras','Anne','K','1991-11-15','501',NULL,NULL),('105','Johnson','Alice','K','1997-02-01','502',NULL,NULL),('106','Smithfield','William',NULL,'2008-06-22','500',NULL,NULL),('107','Alonzo','Maria','D','1997-10-10','501',NULL,NULL),('108','Washington','Ralph','B','1995-08-22','501',NULL,NULL),('109','Smith','Larry','W','2001-07-18','501',NULL,NULL);
 /*!40000 ALTER TABLE `EMP_2` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -205,4 +207,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-05-01 19:20:26
+-- Dump completed on 2026-05-01 19:24:48
