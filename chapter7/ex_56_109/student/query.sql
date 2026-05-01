ead8aeeb6180c200a71d8adb545a3fec53927acd
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-05-01 17:51:27 +0000

    Testing Fri May 01 2026 17:51:27 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,10 +1,4 @@
 SELECT 
-    W.AU_ID,
-    COUNT(*) AS "Books Written"
+    SUM(BOOK_COST) AS "Library Value"
 FROM 
-    WRITES W
-GROUP BY 
-    W.AU_ID
-ORDER BY 
-    COUNT(*) DESC,
-    W.AU_ID ASC;
+    BOOK;
