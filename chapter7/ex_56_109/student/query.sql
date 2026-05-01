cc0c486c1e13de90ab4034bc80609a28d99f915b
hadley-t25 <hadley.turner028@topper.wku.edu>
2026-05-01 17:37:34 +0000

    Testing Fri May 01 2026 17:37:34 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex_56_109/student/query.sql
+++ chapter7/ex_56_109/student/query.sql
@@ -1,10 +1,4 @@
 SELECT 
-    AU_ID,
-    AU_FNAME,
-    AU_LNAME,
-    AU_BIRTHYEAR
+    COUNT(*) AS "Number of Books"
 FROM 
-    AUTHOR
-ORDER BY 
-    AU_BIRTHYEAR DESC,
-    AU_LNAME ASC;
+    BOOK;
